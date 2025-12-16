4ecb0b232762de734ab361bceb07fc26367d4728
qfioofa <penavana1@gmail.com>
2025-12-16 19:46:11 +0300

    Clean up

--- lua/qfioofa/pluginManager/init.lua
+++ lua/qfioofa/pluginManager/init.lua
@@ -3,8 +3,6 @@ local saveInit = utils.saveInit
 local root = utils.roots.pluginManager
 
 local function main()
-	local lazy = require("lazy")
-
 	saveInit(root .. "initPlugins")
 end
 
--- lua/qfioofa/theme/mainTheme.lua
+++
@@ -1,3 +0,0 @@
-local function main() end
-
-return main
