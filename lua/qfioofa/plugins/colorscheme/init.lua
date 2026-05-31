1d44c36b60f020dae50d49e3b420a7e03ae3a2e3
qFioofa <penavana1@gmail.com>
2026-05-31 12:53:57 +0300

    Added other colorschemes

--- lua/qfioofa/plugins/colorscheme/init.lua
+++ lua/qfioofa/plugins/colorscheme/init.lua
@@ -1,37 +1,54 @@
 -- Active colorscheme. Alternatives kept below for quick switching.
+-- List of colorthemes:
+--    yugen-ash
+--    gruvbox
+--    yugen
+--    koda
+--    vscode
 return {
 	{
 		"qfioofa/yugen-ash.nvim",
 		lazy = false,
 		priority = 1000,
+		opts = {
+			variant = "muted",
+		},
 		config = function()
-			-- require("yugen-ash").setup({ variant = "muted" })
+			-- require("yugen-ash").setup({
+			-- 	variant = "muted",
+			-- })
 			vim.cmd("colorscheme yugen-ash")
 		end,
 	},
-
-	-- {
-	-- 	"jellisonleao/gruvbox.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.cmd("colorscheme gruvbox")
-	-- 	end,
-	-- },
-	-- {
-	-- 	"bettervim/yugen",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.cmd("colorscheme yugen")
-	-- 	end,
-	-- },
-	-- {
-	-- 	"rose-pine/neovim",
-	-- 	name = "rose-pine",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("rose-pine").setup({ variant = "main" })
-	-- 		vim.cmd("colorscheme rose-pine")
-	-- 	end,
-	-- },
+	{
+		"oskarnurm/koda.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			theme = {
+				dark = "dark",
+				light = "glade",
+			},
+		},
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"bettervim/yugen",
+		priority = 1000,
+	},
+	{
+		"rose-pine/neovim",
+		lazy = false,
+		name = "rose-pine",
+		priority = 1000,
+	},
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1000,
+	},
 }
