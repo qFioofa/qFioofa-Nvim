f79819c74579d00b4edae734fc9ec72c3b465cf4
qFioofa <penavana1@gmail.com>
2026-05-31 12:59:59 +0300

    Lualine plugin general structer & removed hardcoded colors

--- lua/qfioofa/plugins/lualine/options.lua
+++ lua/qfioofa/plugins/lualine/options.lua
@@ -1,66 +1,7 @@
-local s = {
-	b = { bg = "#bc735c", fg = "#fafafa" },
-	c = { bg = "#151515", fg = "#fafafa" },
-
-	y = { bg = "#303030", fg = "#fafafa" },
-	x = { bg = "#000000", fg = "#fafafa" },
-}
-
 return {
 	options = {
-		theme = {
-			normal = {
-				a = { bg = "#bf616a", fg = "#fafafa" },
-				b = s.b,
-				c = s.c,
-
-				y = s.y,
-				x = s.x,
-			},
-
-			insert = {
-				a = { bg = "#9db89c", fg = "#303030" },
-				b = s.b,
-				c = s.c,
-
-				y = s.y,
-				x = s.x,
-			},
-			visual = {
-				a = { bg = "#D4A017", fg = "#303030" },
-				b = s.b,
-				c = s.c,
-
-				y = s.y,
-				x = s.x,
-			},
-			replace = {
-				a = { bg = "#8dd3c3", fg = "#303030" },
-				b = s.b,
-				c = s.c,
-
-				y = s.y,
-				x = s.x,
-			},
-			command = {
-				a = { bg = "#c678dd", fg = "#303030" },
-				b = s.b,
-				c = s.c,
-
-				y = s.y,
-				x = s.x,
-			},
-			terminal = {
-				a = { bg = "#7A7A7A", fg = "#fafafa" },
-				b = s.b,
-				c = s.c,
-
-				y = s.y,
-				x = s.x,
-			},
-		},
 		component_separators = "",
-		section_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "NvimTree" },
 		always_divide_middle = true,
 	},
@@ -68,16 +9,16 @@ return {
 		lualine_a = {
 			{
 				"mode",
-				icon = "",
-				separator = { left = "", right = "" },
+				icon = "",
+				separator = { left = "", right = "" },
 				right_padding = 2,
 			},
 		},
 		lualine_b = {
 			{
 				"branch",
-				icon = "",
-				separator = { left = "", right = "" },
+				icon = "",
+				separator = { left = "", right = "" },
 				right_padding = 2,
 			},
 		},
@@ -87,16 +28,16 @@ return {
 		lualine_x = {
 			{
 				"diagnostics",
-				icon = " ",
-				separator = { left = "", right = "" },
+				icon = " ",
+				separator = { left = "", right = "" },
 				right_padding = 2,
 			},
 		},
 		lualine_y = {
 			{
 				"filename",
-				icon = "",
-				separator = { left = "", right = "" },
+				icon = "",
+				separator = { left = "", right = "" },
 				-- right_padding = 2,
 				symbols = {
 					modified = "",
@@ -109,8 +50,8 @@ return {
 		lualine_z = {
 			{
 				"location",
-				icon = "",
-				separator = { left = "", right = "" },
+				icon = "",
+				separator = { left = "", right = "" },
 				left_padding = 2,
 				right_padding = 2,
 			},
