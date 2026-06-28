4fdaefa395b620e91fad193330699373506eaeed
qFioofa <penavana1@gmail.com>
2026-06-28 13:12:34 +0300

    improved markdown

--- lua/qfioofa/plugins/render-markdown/init.lua
+++ lua/qfioofa/plugins/render-markdown/init.lua
@@ -5,4 +5,30 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-mini/mini.nvim",
 	},
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {
+		heading = {
+			width = "block",
+			left_pad = 0,
+			right_pad = 1,
+		},
+		code = {
+			width = "block",
+			right_pad = 1,
+		},
+		indent = { enabled = false },
+		pipe_table = {
+			preset = "round",
+			cell = "padded",
+			alignment_indicator = "─",
+		},
+	},
+	keys = {
+		{
+			"<leader>tm",
+			"<cmd>RenderMarkdown toggle<cr>",
+			desc = "Toggle markdown render",
+		},
+	},
 }
